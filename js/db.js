51e41c9a963182b79461391a358c2e41e5e6dad2
Hamza <hamza_ayachi@yahoo.fr>
2019-11-03 18:24:31 +0400

    Providing offline data using firestore and IndexedDB

--- js/db.js
+++ js/db.js
@@ -1,3 +1,16 @@
+// enable offline data
+db.enablePersistence()
+  .catch(function(err) {
+    if (err.code == 'failed-precondition') {
+      // probably multible tabs open at once
+      console.log('persistance failed');
+    } else if (err.code == 'unimplemented') {
+      // lack of browser support for the feature
+      console.log('persistance not available');
+    }
+  });
+
+// real-time listener
 db.collection('rides').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
       if(change.type === 'added'){
